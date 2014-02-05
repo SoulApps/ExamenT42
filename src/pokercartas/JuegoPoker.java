50f97da83cb2f1b028f17a63e0c2b7f97ce91083
usuario <usuario@RamnPC>
2014-02-05 15:00:25 +0100

    fase 2. v0.2 No estable

---
+++ src/pokercartas/JuegoPoker.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pokercartas;
+
+/**
+ *
+ * @author usuario
+ */
+//
+BarajaFrancesa 
+public class JuegoPoker {
+    public final int NADA = 0;
+    public final int PAREJA = 2;
+    public final int TRIO = 3;
+    public final int POKER = 4;
+    public final int REPOKER = 5;
+
+    
+    public int getCombinacion(CartaFrancesa carta1,CartaFrancesa carta2,CartaFrancesa carta3,CartaFrancesa carta4, CartaFrancesa carta5){
+    
+
+    }
+}
